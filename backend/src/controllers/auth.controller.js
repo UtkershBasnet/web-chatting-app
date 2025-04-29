664e89c499384962600d1c6c44f52bdcb38d4b97
UtkershBasnet <utkershbasnet@gmail.com>
2025-04-30 02:20:30 +0530

    backend and database initialised

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export function login(req, res){
+  res.send('Login page');
+}
+
+export function signup(req, res){
+    res.send('Signup page');
+  }
+
+export function logout(req, res){
+    res.send('Logout page');
+  }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
-
+import authRoutes from './routes/auth.route.js';
+import dotenv from 'dotenv';
+import { connectDB } from './lib/db.js';
+dotenv.config();
 const app = express();
 
-app.listen(5001, () => {
-  console.log('Server is running on port 5001');
+app.use(express.json());
+app.use("/api/auth" ,authRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port ' + process.env.PORT);
+  connectDB();
+  console.log('MongoDB connected');
 }
 );
