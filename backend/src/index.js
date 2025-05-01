2652f445d647a2f9d3a784d1c6886adb17af6877
UtkershBasnet <utkershbasnet@gmail.com>
2025-05-01 23:42:43 +0530

    frontend initialised and backend completed for signing routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/auth" ,authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port ' + process.env.PORT);
