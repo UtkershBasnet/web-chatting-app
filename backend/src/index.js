4fe37cd700df22320c4edd317cab13cda0343d70
UtkershBasnet <utkershbasnet@gmail.com>
2025-05-04 01:35:43 +0530

    changes to deploy in production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,7 +6,11 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import { connectDB } from './lib/db.js';
 import cookieParser from 'cookie-parser';
+import path from "path";
 dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -18,8 +22,16 @@ app.use(cookieParser());
 app.use("/api/auth" ,authRoutes);
 app.use("/api/messages", messageRoutes);
 
-server.listen(process.env.PORT, () => {
-  console.log('Server is running on port ' + process.env.PORT);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
+  console.log('Server is running on port ' + PORT);
   connectDB();
   console.log('MongoDB connected');
 }
